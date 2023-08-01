14a0967d2551caf128b7c3c48a3bccd37609b9e3
Igor <ygarik@mail.ru>
2023-08-01 20:57:50 +0300

    Решили первую задачу

---
+++ Task_1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+int result = number * number;
+Console.WriteLine("Наш результат:" + result);
+
