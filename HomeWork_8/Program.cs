c448cd0929da9b71442ee118512843aad9c70779
Igor <ygarik@mail.ru>
2023-08-04 17:28:47 +0300

    Сделал №8

---
+++ HomeWork_8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.WriteLine("Введите число");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+int chetnoe = 1;
+while (chetnoe < number)
+    {if (chetnoe % 2 == 0) Console.WriteLine(chetnoe);
+    chetnoe = chetnoe + 1;
+    }
---
+++ Homework_6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.WriteLine("Введите число");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+if (number%2 == 0) Console.WriteLine("число " + number + " является четным");
+else  Console.WriteLine("число " + number + " является нечетным");
